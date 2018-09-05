af5ed11c75760c391339d71ad94645ed2eb7e5d9
Akshay Phadke <aphadke93@gmail.com>
2018-09-04 19:13:47 -0700

    Modify configuration of curses application

--- hiss.py
+++ hiss.py
@@ -3,9 +3,22 @@ import curses
 def initialize():
   return curses.initscr()
 
+def customize_curse(stdscr):
+  curses.noecho()
+  curses.cbreak()
+  stdscr.keypad(1)
+
+def revert_customization(stdscr):
+  stdscr.keypad(0)
+  curses.nocbreak()
+  curses.echo()
+
 def terminate():
   curses.endwin()
 
 if __name__ == '__main__':
   stdscr = initialize()
+  customize_curse(stdscr)
+  revert_customization(stdscr)
   terminate()
+
